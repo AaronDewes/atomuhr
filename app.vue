3a36f90b0b2541785b91b5150fb501fbcc1127da
Aaron Dewes <aaron.dewes@protonmail.com>
2022-09-28 07:25:55 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -29,7 +29,7 @@ export default defineComponent({
       return format(this.date, 'ppp')
     },
     host() {
-      if (process.browser) {
+      if (window && window?.location) {
         return window.location.host;
       }
       // Fallback during SSR
